e8f4b01c6569f281cea770fcf1dc0ce490e1d617
Dong Liu <winter.solstice1219@gmail.com>
2021-05-15 10:00:10 +0530

    Create SafeMoon_Contract_Clone.sol

---
+++ SafeMoon_Contract_Clone.sol
@@ -0,0 +1,8 @@
+You need contract like SafeMoon with features:
+
+   3% fee auto add to the liquidity pool to locked forever when selling
+   2% fee auto distribute to all holders
+   I created a black hole so token will deflate itself in supply with every transaction
+   50% Supply is burned at start.
+   
+   Please contact me on telegram @TokoAgus
